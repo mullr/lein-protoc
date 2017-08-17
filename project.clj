267d03dbea2f75a00a4c7deef2be9cf5f9f623f8
Ben Sima <bensima@gmail.com>
2017-08-16 17:02:43 -0700

    Add lein-codox

--- project.clj
+++ project.clj
@@ -6,4 +6,9 @@
   :min-lein-version "2.0.0"
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
-  :profiles {:dev {:dependencies [[com.google.protobuf/protobuf-java "3.3.1"]]}})
+  :profiles {:dev {:dependencies [[com.google.protobuf/protobuf-java "3.3.1"]]}}
+  :plugins [[lein-codox "0.10.3"]]
+  :codox
+  {:output-path "docs"
+   :metadata {:doc/format :markdown}
+   :source-uri "https://github.com/LiaisonTechnologies/lein-protoc/blob/{version}/{filepath}/#L{line}"})
